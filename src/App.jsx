c34cf053ded0077c20206a9a1521a738d5d9780f
al1asgar <al1asgar@hotmail.com>
2024-07-06 14:10:59 -0400

    Top Scroll Bar Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
-import styles from './App.module.css'
-import { Education } from './components/Education/Education'
-import { Hero } from './components/Hero/Hero'
-import { Navbar } from './components/Navbar/Navbar'
-import { Experience } from './components/Experience/Experience'
-import { Projects } from './components/Projects/Projects'
-import { Contact } from './components/Contact/Contact'
+import styles from './App.module.css';
+import { Education } from './components/Education/Education';
+import { Hero } from './components/Hero/Hero';
+import { Navbar } from './components/Navbar/Navbar';
+import { Experience } from './components/Experience/Experience';
+import { Projects } from './components/Projects/Projects';
+import { Contact } from './components/Contact/Contact';
+import { motion, useScroll, useSpring } from "framer-motion";
 
 function App() {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  });
 
-
-  return (<div className={styles.App}> <Navbar />
-    <Hero/>
-    <Experience/>
-    <Projects/>
-    <Education/>
-    <Contact/>
-  </div>)
+  return (
+    <div className={styles.App}>
+      <motion.div className={styles.progressBar} style={{ scaleX }} />
+      <Navbar />
+      <Hero />
+      <Experience />
+      <Projects />
+      <Education />
+      <Contact />
+      
+    </div>
+  );
 }
 
-export default App
+export default App;
